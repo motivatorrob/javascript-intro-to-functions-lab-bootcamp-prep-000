facda3eb38a9977cf1b94b70a40ba4d3477d9185
robdjohns <robdjohns@gmail.com>
2018-09-15 16:55:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ function logWhisper(string)  {
 
 function sayHiToGrandma(string)  {
   string.toLowerCase()===string
+  var upppercase = string
   return `I can\'t hear you!`
   string.toUpperCase()===string
+  var lowercase = string
   return `YES INDEED!`
   string==='I love you, Grandma.'
+  var love = string
   return `I love you, too.`
 }
 
